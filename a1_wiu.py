30ba69930fbb2ae083166d61a7938921bf792877
wiu1124 <71338785+wiu1124@users.noreply.github.com>
2020-10-13 21:11:50 -0400

    Update a1_wiu.py

--- a1_wiu.py
+++ a1_wiu.py
@@ -26,7 +26,7 @@ def usage():
 
 def sanitize(obj1, obj2):
     obj1 = user_raw_data.replace('/', '').replace('.', '').replace('-','')
-    obj2 = allow_chars
+    obj2 = allow_chars.replace(user_raw_data, '')
     return obj1
 
 def size_check(obj, intobj):
@@ -57,36 +57,47 @@ def leap_year(obj3):
      return False
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-     usage()
-     sys.exit()
-    month_name = ['Jan','Feb','Mar','Apr','May','Jun',
-                  'Jul','Aug','Sep','Oct','Nov','Dec']
-    days_in_month = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30,
-                     7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
-    user_raw_data = sys.argv[1]
+   # step 1
+   if len(sys.argv) != 2:
+      usage()
+      sys.exit()
+   # step 2
+   month_name = ['Jan','Feb','Mar','Apr','May','Jun',
+                 'Jul','Aug','Sep','Oct','Nov','Dec']
+   days_in_month = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30,
+                    7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+   user_raw_data = sys.argv[1]
+   # step 3
+   allow_chars = '0123456789'
+   dob = sanitize(user_raw_data, allow_chars)
+   # step 4
+   result = size_check(dob,8)
+   if result == False:
+       print("Error 09: wrong date entered")
+       sys.exit()
+   # step 5
+   year = int(dob[0:4])
+   month = int(dob[4:6])
+   day = int(dob[6:])
+   # step 6
+   result = range_check(year,(1900,9999))
+   if result == False:
+       print("Error 10: year out of range, must be 1900 or later")
+       sys.exit()
+   result = range_check(month,(1,12))
+   if result == False:
+       print("Error 02: Wrong month entered")
+       sys.exit()
+      result = leap_year(year)
+   if result == True:
+       days_in_month[2] = 29
+   result = range_check(day, (1, days_in_month[month]))
+   if result == False:
+       print("Error 03: wrong day entered")
+       sys.exit()
+   # step 7
+   new_dob = str(month_name[month - 1])+' '+ str(day)+', '+str(year)
+   # step 8
+   print(new_dob)
 
-    allow_chars = '0123456789'
-    dob = sanitize(user_raw_data, allow_chars)
-    print('Sanitized user data:', dob)
-    #size_check
-    result = size_check(dob,8)
-    if result == False:
-     print("Error 09: wrong data entered")
-          sys.exit()
-    year = dob[0:4]
-    month = int(dob[4:6])
-    day = dob[6:]
-    #range_check
-    result = range_check(year,(1900,9999))
-    if result == False:
-     print("Error 10: year out of range, must be 1900 or later")
-     sys.exit()
-    result = range_check(month,(1,12))
-    if result == False:
-     print("Error 02: Wrong month entered")
-     sys.exit()
-    new_dob = month_name[month - 1] + ' ' + day + ', ' + year
-    print("Your date of birth is:", new_dob)
-
-
+        
